04eaddcbeb08b281dd13b32a6b3c5dba71821f3a
vlee791 <vlee791@terpmail.umd.edu>
2024-03-27 20:57:51 -0400

    New .py route and favorite course html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -7,21 +7,33 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World from Victor Lee! I am adding my first code change.'
 
+
 @app.route('/hello')
 def hello():
     return render_template('hello.html')
+
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+
 @app.route('/about-css')
 def about_css():
     return render_template('about-css.html')
 
+
+@app.route('/favorite-course')
+def favorite_course():
+    subject = request.args.get('subject')
+    course_number = request.args.get('course_number')
+    return render_template('favorite-course.html', subject=subject, course_number=course_number)
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
 if __name__ == '__main__':
     app.run()
-
-
-
-
